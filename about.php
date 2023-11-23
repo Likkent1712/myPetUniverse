7dd7f293d57c2dd274aba59e7793f173439d3fe8
Your Name <you@example.com>
2023-11-24 01:22:09 +0800

    main

---
+++ about.php
@@ -0,0 +1,21 @@
+<section class="py-5">
+    <div class="container">
+        <div class="card rounded-0">
+            <div class="card-body">
+                <h1 class="text-center">About Us</h1>
+                <hr>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="./uploads/1624240440_paw.png" alt="" class="img-fluid" width="50%">
+                    </div>
+                    <p></p>
+                        <p class="text-justify">We are a team of developers dedicated to creating high-quality software solutions for our clients. Our expertise includes web development, mobile app development, and software consulting.</p>
+                        <p class="text-justify">We are a team of developers dedicated to creating high-quality software solutions for our clients. Our expertise includes web development, mobile app development, and software consulting.</p>
+                        <p class="text-justify">We are a team of developers dedicated to creating high-quality software solutions for our clients. Our expertise includes web development, mobile app development, and software consulting.</p>
+                </div>
+                <hr>
+                <?php include "about.html" ?>
+            </div>
+        </div>
+    </div>
+</section>
